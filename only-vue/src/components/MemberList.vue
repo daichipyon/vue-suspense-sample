024cc2429c1acb1908f8c99a97161547a3187b3c
daichipyon <33198528+daichipyon@users.noreply.github.com>
2024-01-02 16:36:49 +0000

    defineComponentを使ってやり直し

---
+++ only-vue/src/components/MemberList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>MemberList</h1>
+        <Suspense>
+            <template #default>
+                <AsyncMemberList />
+            </template>
+            <template #fallback>
+                <p>Loading...</p>
+            </template>
+        </Suspense>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
+const AsyncMemberList = defineAsyncComponent(() => import('../components/AsyncMemberList.vue'))
+
+</script>
