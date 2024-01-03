97b597635612b828700baeb253b8d4f515a4db42
daichipyon <33198528+daichipyon@users.noreply.github.com>
2024-01-03 05:06:58 +0000

    not client onlyをなくす

--- app.vue
+++ app.vue
@@ -3,10 +3,15 @@
     <h1>client only</h1>
     <ClientOnly>
       <MemberList />
+      <template #fallback>
+        <p>this is rendered by serverside and will be replaced by client side</p>
+      </template>
+
     </ClientOnly>
 
-    <h1>not client only</h1>
-    <MemberList />
 
+    <!-- <h1>not client only</h1>
+    <MemberList />
+     -->
   </div>
 </template>
